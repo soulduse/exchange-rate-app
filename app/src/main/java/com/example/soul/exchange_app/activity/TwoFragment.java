2c543e0327a0f948c6e51e183b612cc3ba457d0e
soulduse <soulduse@naver.com>
2017-04-27 14:33:51 +0900

    layout is editing

--- app/src/main/java/com/example/soul/exchange_app/activity/TwoFragment.java
+++ app/src/main/java/com/example/soul/exchange_app/activity/TwoFragment.java
@@ -25,6 +25,7 @@ public class TwoFragment  extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_two, container, false);
+//        return inflater.inflate(R.layout.fragment_two, container, false);
+        return inflater.inflate(R.layout.calcu_layout, container, false);
     }
 }
