5cd9ea87676dfe3f389ccc4ca35de842572bfdce
soulduse <soulduse@naver.com>
2017-06-02 19:27:10 +0900

    made Notification test class

--- app/src/main/java/com/example/soul/exchange_app/activity/ThreeFragment.java
+++ app/src/main/java/com/example/soul/exchange_app/activity/ThreeFragment.java
@@ -1,11 +1,18 @@
 package com.example.soul.exchange_app.activity;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.soul.exchange_app.R;
 
@@ -14,17 +21,46 @@ import com.example.soul.exchange_app.R;
  */
 
 public class ThreeFragment  extends Fragment {
+
+    private NotificationManager notificationManager;
+    private NotificationCompat.Builder notificationBuilder;
+    private Button button;
+
     public ThreeFragment() {
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(),
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notificationManager = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationBuilder = new NotificationCompat.Builder(getContext())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("오늘의 환율")
+                .setContentText("미국 달러 상승")
+                .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setContentIntent(pendingIntent);
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_three, container, false);
+        View view = inflater.inflate(R.layout.fragment_three, container, false);
+
+        button = (Button)view.findViewById(R.id.noti_test);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notificationManager.notify(0, notificationBuilder.build());
+            }
+        });
+
+        return view;
     }
 }
