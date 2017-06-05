14c1c9333d97307791c163af9e4a4d8c3ab1b018
soulduse <soulduse@naver.com>
2017-06-05 19:59:58 +0900

    UI 바인딩으로 교체함 - 바인딩 사용을 익숙하게 하

--- app/src/main/java/com/example/soul/exchange_app/ui/CustomNotiDialog.java
+++ app/src/main/java/com/example/soul/exchange_app/ui/CustomNotiDialog.java
@@ -1,21 +1,25 @@
 package com.example.soul.exchange_app.ui;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.adapters.ToolbarBindingAdapter;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.soul.exchange_app.R;
-import com.example.soul.exchange_app.adapter.DialogAdapter;
 import com.example.soul.exchange_app.adapter.DialogAdapter2;
+import com.example.soul.exchange_app.databinding.DialogNotificationBinding;
+import com.example.soul.exchange_app.model.ExchangeRate;
 import com.example.soul.exchange_app.realm.RealmController;
+import com.example.soul.exchange_app.util.MoneyUtil;
 
 import io.realm.Realm;
 
@@ -28,7 +32,7 @@ public class CustomNotiDialog extends DialogFragment implements AdapterView.OnIt
 
     private Realm realm;
     private RealmController realmController;
-    private DialogAdapter2 adapter;
+    private DialogNotificationBinding binding;
 
     /**
      * Create a new instance of CountryDialog, providing "num"
@@ -55,29 +59,74 @@ public class CustomNotiDialog extends DialogFragment implements AdapterView.OnIt
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.dialog_notification, container, false);
-        Spinner countrySpinner = (Spinner) v.findViewById(R.id.spinner);
-        Spinner exchangeSpinner = (Spinner) v.findViewById(R.id.spinner2);
-        TextView aboveOrbelow = (TextView)v.findViewById(R.id.aboveOrbelowTxt);
-        TextView addAlarm = (TextView)v.findViewById(R.id.addAlarm);
-        TextView cancelAlarm = (TextView)v.findViewById(R.id.cancelAlarm);
-        EditText alarmPrice = (EditText)v.findViewById(R.id.alarmPrice);
+        binding = DataBindingUtil.inflate(inflater, R.layout.dialog_notification, container, false);
+        binding.setDialog(this);
 
-        //setadapter
-        adapter = new DialogAdapter2(realmController.getExchangeRateExceptKorea(), getActivity());
+        DialogAdapter2 countryAdapter               = new DialogAdapter2(realmController.getExchangeRateExceptKorea(), getActivity());
+        ArrayAdapter<CharSequence> exchangeAdapter  = ArrayAdapter.createFromResource(getContext(),
+                R.array.price_options, android.R.layout.simple_spinner_item);
+        exchangeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        countrySpinner.setOnItemSelectedListener(this);
-        countrySpinner.setAdapter(adapter);
-        exchangeSpinner.setOnItemSelectedListener(this);
+        binding.spinner.setAdapter(countryAdapter);
+        binding.spinner2.setAdapter(exchangeAdapter);
 
-
-        return v;
+        View view = binding.getRoot();
+        setListener();
+        return view;
     }
+
+    private void setListener(){
+        binding.spinner.setOnItemSelectedListener(this);
+        binding.spinner2.setOnItemSelectedListener(this);
+
+        binding.aboveOrbelowTxt.setOnClickListener(clickListener);
+        binding.addAlarm.setOnClickListener(clickListener);
+        binding.deleteAlarm.setOnClickListener(clickListener);
+    }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                // 이상 이하 switch
+                case R.id.aboveOrbelowTxt:
+
+                    String text = binding.aboveOrbelowTxt.getText().equals(getString(R.string.above))
+                            ? getString(R.string.below) : getString(R.string.above);
+                    binding.aboveOrbelowTxt.setText(text);
+                    break;
+
+                // 환율 알림 제거
+                case R.id.deleteAlarm:
+                    Toast.makeText(getContext(), "삭제", Toast.LENGTH_SHORT).show();
+                    dismiss();
+                    break;
+                // 환율 알림 추가
+                case R.id.addAlarm:
+                    String priceText = binding.alarmPrice.getText().toString();
+                    if(priceText == null || priceText.isEmpty()){
+                        Toast.makeText(getContext(), R.string.warning_empty_price, Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+
+                    ExchangeRate exchangeRate = (ExchangeRate) binding.spinner.getSelectedItem();
+                    // true = 이상, false = 이하
+                    boolean state = binding.aboveOrbelowTxt.getText().toString().equals("이상") ? true : false;
+                    // price가 숫자인지에 대한 검증은 안해도된다 왜냐하면 EditText의 데이터 타입을 숫자로 설정 해놓았기 때문.
+                    double price = Double.parseDouble(priceText);
+                    int standardExchange = binding.spinner2.getSelectedItemPosition();
+
+                    realmController.addAlarm(exchangeRate, state, price, standardExchange);
+                    break;
+            }
+        }
+    };
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        Toast.makeText(getContext(), "", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
