a69ec2841b77771591554741b1f22fc24c11639d
soulduse <soulduse@naver.com>
2017-06-05 19:52:08 +0900

    AlarmModel 클래스 생

---
+++ app/src/main/java/com/example/soul/exchange_app/model/AlarmModel.java
@@ -0,0 +1,46 @@
+package com.example.soul.exchange_app.model;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by soul on 2017. 6. 5..
+ */
+
+public class AlarmModel extends RealmObject{
+    private ExchangeRate exchangeRate;
+    private int standardExchange;
+    private double price;
+    private boolean aboveOrbelow;
+
+    public ExchangeRate getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public void setExchangeRate(ExchangeRate exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public int getStandardExchange() {
+        return standardExchange;
+    }
+
+    public void setStandardExchange(int standardExchange) {
+        this.standardExchange = standardExchange;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAboveOrbelow() {
+        return aboveOrbelow;
+    }
+
+    public void setAboveOrbelow(boolean aboveOrbelow) {
+        this.aboveOrbelow = aboveOrbelow;
+    }
+}
