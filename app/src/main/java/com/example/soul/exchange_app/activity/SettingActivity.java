db016cb57bd3f236ba28c689b2c006e363d43310
soulduse <soulduse@naver.com>
2017-06-26 16:56:55 +0900

    preferenceFragment에 Actionbar 표시 안되는 문제 있어서 해결

--- app/src/main/java/com/example/soul/exchange_app/activity/SettingActivity.java
+++ app/src/main/java/com/example/soul/exchange_app/activity/SettingActivity.java
@@ -2,35 +2,52 @@ package com.example.soul.exchange_app.activity;
 
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.soul.exchange_app.R;
 
-import java.util.List;
 
 /**
  * Created by soul on 2017. 6. 22..
  */
 
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_setting);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setDisplayShowHomeEnabled(true);
-//        setTitle(getResources().getString(R.string.setting_actionbar_name));
 
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
 
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        ViewGroup contentView = (ViewGroup) LayoutInflater.from(this).inflate(
+                R.layout.activity_setting, new CoordinatorLayout(this), false);
+
+        toolbar = (Toolbar) contentView.findViewById(R.id.toolbar_setting);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        setTitle(getResources().getString(R.string.setting_actionbar_name));
+
+        ViewGroup contentWrapper = (ViewGroup) contentView
+                .findViewById(R.id.content);
+        LayoutInflater.from(this).inflate(layoutResID, contentWrapper, true);
+
+        getWindow().setContentView(contentView);
     }
 
     // PreferenceFragment 클래스 사용
@@ -41,6 +58,8 @@ public class SettingActivity extends PreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
         }
+
+
     }
 
 }
