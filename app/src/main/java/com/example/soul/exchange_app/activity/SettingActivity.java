8a44f5209b8f6c79b7b4c8139bd0bf0049274c95
soulduse <soulduse@naver.com>
2017-06-30 00:29:42 +0900

    옵션 UI 구조 잡음 - 코드 직관적으로 변

--- app/src/main/java/com/example/soul/exchange_app/activity/SettingActivity.java
+++ app/src/main/java/com/example/soul/exchange_app/activity/SettingActivity.java
@@ -1,18 +1,17 @@
 package com.example.soul.exchange_app.activity;
 
 
-import android.content.Context;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.example.soul.exchange_app.R;
 
@@ -21,37 +20,28 @@ import com.example.soul.exchange_app.R;
  * Created by soul on 2017. 6. 22..
  */
 
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
 
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
 
-    }
 
-    @Override
-    public void setContentView(int layoutResID) {
-
-        ViewGroup contentView = (ViewGroup) LayoutInflater.from(this).inflate(
-                R.layout.activity_setting, new CoordinatorLayout(this), false);
-
-        toolbar = (Toolbar) contentView.findViewById(R.id.toolbar_setting);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar = (Toolbar)findViewById(R.id.toolbar_setting);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         setTitle(getResources().getString(R.string.setting_actionbar_name));
 
-        ViewGroup contentWrapper = (ViewGroup) contentView
-                .findViewById(R.id.content);
-        LayoutInflater.from(this).inflate(layoutResID, contentWrapper, true);
+        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.content_preference);
 
-        getWindow().setContentView(contentView);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.content_preference, new SettingsFragment())
+                .commit();
     }
 
     // PreferenceFragment 클래스 사용
