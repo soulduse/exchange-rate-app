9be4fd0742a3876e042af4296e5de0366b0a1bb6
soulduse <soulduse@naver.com>
2017-07-31 23:19:59 +0900

    RestartService 구현 - 브로드캐스트리시

---
+++ app/src/main/java/com/example/soul/exchange_app/activity/RestartService.java
@@ -0,0 +1,39 @@
+package com.example.soul.exchange_app.activity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by soul on 2017. 7. 31..
+ */
+
+public class RestartService extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("000 RestartService" , "RestartService called : " + intent.getAction());
+
+        /**
+         * 서비스 죽일때 알람으로 다시 서비스 등록
+         */
+        if(intent.getAction().equals("ACTION.RESTART.AlarmService")){
+
+            Log.i("000 RestartService" ,"ACTION.RESTART.AlarmService " );
+
+            Intent i = new Intent(context,AlarmService.class);
+            context.startService(i);
+        }
+
+        /**
+         * 폰 재시작 할때 서비스 등록
+         */
+        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+
+            Log.i("RestartService" , "ACTION_BOOT_COMPLETED" );
+            Intent i = new Intent(context,AlarmService.class);
+            context.startService(i);
+
+        }
+    }
+}
